e6392410dda8ef647ba0870fbb1c0b048910f60e
Matheus Plessmann <maplessmann@gmail.com>
2020-07-21 11:19:04 -0300

    Add image example

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,36 @@
-const IndexPage = () => <h1>Home page</h1>
+import { useStaticQuery, graphql } from 'gatsby'
+
+import Img from 'gatsby-image'
+import Link from '../components/Link'
+
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(absolutePath: { regex: "/example-image.jpg/" }) {
+        childImageSharp {
+          fixed(width: 600, quality: 90) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <div className="home-page">
+      <h1>Home page</h1>
+
+      <h2>Images with lazy loading</h2>
+      <Img fixed={data.file.childImageSharp.fixed} />
+      <p>
+        See the{' '}
+        <Link external to="https://www.gatsbyjs.org/docs/gatsby-image/">
+          gatsby-image
+        </Link>{' '}
+        docs for more information
+      </p>
+    </div>
+  )
+}
 
 export default IndexPage
