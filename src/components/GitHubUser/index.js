5befb69232cb1532ce48efd7f35592bdecdfa086
Matheus Plessmann <maplessmann@gmail.com>
2020-12-02 20:12:39 -0300

    Adjust GitHubUser style

--- src/components/GitHubUser/index.js
+++ src/components/GitHubUser/index.js
@@ -6,16 +6,16 @@ const GitHubUser = () => {
   const { userData } = useGitHubUser()
 
   return (
-    <Link className="github-user" href={userData.html_url} external>
+    <div className="github-user">
       <header className="header">
         <img className="avatar" src={userData.avatar_url} alt="GitHub avatar" />
-        <div className="names">
+        <Link className="names" href={userData.html_url} external>
           <span className="name">{userData.name}</span>
           <span className="login">@{userData.login}</span>
-        </div>
+        </Link>
       </header>
       <p className="bio">{userData.bio}</p>
-    </Link>
+    </div>
   )
 }
 
