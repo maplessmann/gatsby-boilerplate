55d7ecf4355c3ab63b5c0a4d40744529874acaa2
Matheus Plessmann <maplessmann@gmail.com>
2020-12-15 13:07:38 -0300

    Resize image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import Page from 'components/Page'
 import GithubSearch from 'components/GithubSearch'
+import ImageExample from 'components/ImageExample'
 
 const HomePage = () => {
   return (
     <Page>
       <GithubSearch />
+      <ImageExample />
     </Page>
   )
 }
