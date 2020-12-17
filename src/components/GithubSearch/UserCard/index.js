d72bf8e13351cded5ad897cbb91ac8aac880497f
Matheus Plessmann <maplessmann@gmail.com>
2020-12-17 02:22:04 -0300

    Avoid errors related to non-existing properties when accessing API data

--- src/components/GithubSearch/UserCard/index.js
+++ src/components/GithubSearch/UserCard/index.js
@@ -6,13 +6,13 @@ const UserCard = ({ userData }) => {
   return (
     <div className="user-card">
       <header className="header">
-        <img className="avatar" src={userData.avatarUrl} alt="GitHub avatar" />
-        <Link className="names" to={userData.url} external>
-          <span className="name">{userData.name}</span>
-          <span className="login">@{userData.login}</span>
+        <img className="avatar" src={userData?.avatarUrl} alt="GitHub avatar" />
+        <Link className="names" to={userData?.url} external>
+          <span className="name">{userData?.name}</span>
+          <span className="login">@{userData?.login}</span>
         </Link>
       </header>
-      <p className="bio">{userData.bio}</p>
+      <p className="bio">{userData?.bio}</p>
     </div>
   )
 }
